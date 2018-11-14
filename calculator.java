4a3100a44fbc57607afd14e0dd3f707fb75d0801
Dhivyapriya K <kdhivyapriya97@gmail.com>
2018-11-14 17:03:28 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,73 @@
+public class Main
+
+{
+	
+public static void main(String[] args) 
+	
+{
+	    
+int num1=Integer.parseInt(args[0]);
+	    
+int num2=Integer.parseInt(args[1]);
+	    
+String opr=args[2];
+	    
+int answer;
+	    
+switch(opr)
+	    
+{
+	        
+case "add":
+	             
+answer=num1+num2;
+	            
+ System.out.println("addtion:"+answer);
+	           
+  break;
+	        
+case "sub":
+	           
+answer=num1-num2;
+	            
+System.out.println("subraction:"+answer);
+	           
+ break;
+	        
+case "mul":
+	            
+answer=num1*num2;
+	            
+System.out.println("multiplication:"+answer);
+	            
+break;
+	        
+case "div":
+	            
+answer=num1/num2;
+	            
+System.out.println("division:"+answer);
+	           
+ break;
+	        
+case "mod":
+	            
+answer=num1%num2;
+	            
+System.out.println("modulo:"+answer);
+	            
+break;
+	         
+default:
+	          
+System.out.println("enter valid operator");
+	        	            
+ }
+	        	             
+	       
+	    
+		
+
+}
+	
+}
